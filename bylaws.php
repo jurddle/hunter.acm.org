5a1ff1f4baa0da0d3b729414a8d9bc0ce667c931
Robert O'Connor <rob@oconnor.ninja>
2014-09-23 23:55:06 -0400

    Add chapter bylaws; this code was written by Alex Rosario, our current chair

---
+++ bylaws.php
@@ -0,0 +1,186 @@
+<?php
+$title = "ACM @ Hunter - Chapter Bylaws";
+$section = 'bylaws';
+require_once(dirname(__FILE__).'/scripts/top.php');
+?>
+
+<h1>Chapter Bylaws</h1>
+
+<p>In the interest of transparency, the Hunter College student chapter of the Association for Computing Machinery makes available a web-formatted copy of the official bylaws that govern this chapter. These bylaws are in accordance with both ACM policy regarding student chapters and with Hunter College Undergraduate Student Government policies.</p>
+<p>If there is a conflict between the actual bylaws and this page (e.g. this page is out of date for any length of time following an approval of updated bylaws), the actual approved bylaws supersede the information on this page.</p>
+<p>This page may also contain annotations regarding unofficial club customs which are not meant to serve as official documentation. They will be marked "NB." and <span class='nb'>colored in blue</span>.</p>
+
+<h2>I. Name</h2>
+<ol class="bl">
+	<li>This organization shall be called the Student Chapter of the Association for Computing Machinery at Hunter College. The Association is also referred to as ACM.</li>
+</ol>
+<p class="nb">NB. For the purposes of convenience, the chapter may be referred to as the "ACM @ Hunter."</p>
+
+<h2>II. Purpose</h2>
+<ol class="bl">
+	<li>The Chapter is organized and will be operated exclusively for educational and scientific purposes and, in furtherance thereof, specific objectives are:
+		<ol>
+			<li>To promote an increased knowledge of the science, design, development, construction, language, and applications of modern computing machinery;</li>
+			<li>To promote a greater interest in computing machinery and its applications; and</li>
+			<li>To provide a means of communication between person having interest in computing machinery.</li>
+		</ol>
+	</li>
+</ol>
+
+<h2>III. Membership</h2>
+<ol class="bl">
+	<li>Membership in the Chapter shall be open to all members and nonmembers of the ACM who are also members of the Hunter College Community, regardless of race, creed, national origin, sex or sexual orientation.</li>
+	<li>Voting membership in the Student Chapter shall be granted to all Chapter members.</li>
+	<li>All members are required to attend all regular scheduled meetings. Excused absences can be granted by any executive board member prior to the meeting. If a member has more than two unexcused absences he/she may be removed from the Chapter.</li>
+	<li>All members are to attend all sponsored functions. Excused absences may be granted by any executive board member prior to the scheduled event. If more than two unexcused absences occur, then the member will be removed from the Chapter.</li>
+</ol>
+
+<h2>IV. Officers</h2>
+<ol class="bl">
+	<li>The officers of this Chapter shall be President, Vice-President, Secretary, and Treasurer. All officers of the Chapter must be members of ACM. This forms the Executive Board.</li>
+	<li>Candidacy for office shall be screened by the election committee, which shall be chaired by the Executive Board of this chapter.</li>
+	<li>In order to be nominated, a member must have been an Executive Board member previously, or have served on a committee prior to candidacy.</li>
+	<li>The officers shall be elected at the annual election meeting. They shall take office at the end of the election semester and serve until their successor takes office at the end of the next election semester.</li>
+	<li>No member may serve in the office of President or Vice-President for more than two consecutive years.</li>
+	<li>Vacancies in any office other than President caused by resignation, removal or disability shall be filled by majority vote of the Executive Board by consultation of the the Election Committee. If the office of President is so vacated, the Vice-President shall immediately assume that office, and the Executive Board, by consultation with the Election Committee shall then fill the vacant office of Vice-President.</li>
+	<li>Any amount of an officer's term used to fill a vacancy shall not be counted towards Section 5 of this article.</li>
+	<li>A vacancy shall be considered filled after a majority vote of the members of the membership.</li>
+</ol>
+
+<h2>V. Duties of Officers and Removal Procedures</h2>
+<h3>President (Chair)</h3>
+<ol class="bl">
+	<li>To preside over meetings of the board.</li>
+	<li>To keep advisor(s) informed of all activities and events.</li>
+	<li>To be the official representative of the organization.</li>
+	<li>To insure proper implementation of budgetary matters.</li>
+	<li>To coordinate the efforts and overall activities of the organization.</li>
+	<li>To coordinate the programming efforts of the organization with other clubs, organizations and programming groups.</li>
+	<li>To bring all major decisions to the board for a vote and be held responsible for those decisions.</li>
+	<li>To meet with advisor(s) at least once a week.</li>
+	<li>To assist in the preparation of budget requests by the Treasurer.</li>
+	<li>To be responsible for the planning, organizing and procedural phase and activities of the board.</li>
+	<li>To have power to veto any decision deemed not to be in the best interest of the board; However any veto may be overridden by two-thirds vote of the Executive Board.</li>
+	<li>To plan, coordinate and implement all aspects of all programs during the year.</li>
+	<li>To preside over Executive Board meetings.</li>
+	<li>The President shall also appoint all committees of this Chapter and committee members.</li>
+</ol>
+<h3>Vice President (Vice Chair)</h3>
+<ol class="bl">
+	<li>To serve as acting President in the absence of the President.</li>
+	<li>To serve as an official representative of the organization in the absence of the President.</li>
+	<li>To meet with advisors in the absence of the President.</li>
+	<li>To plan, coordinate and implement all aspects of all programs during the year.</li>
+	<li>To attend all Executive Board meetings.</li>
+	<li>To perform other duties as delegated by the President.</li>
+	<li>Serve as Chair of the Program Committee.</li>
+</ol>
+<h3>Secretary</h3>
+<ol class="bl">
+	<li>To record and distribute all minutes to all members and advisor(s) within 1 week after the weekly meetings.</li>
+	<li>To keep bulletin boards current and post a calendar of current events.</li>
+	<li>To post a list of meeting schedules, names of members and advisors.</li>
+	<li>To schedule meeting rooms for meetings.</li>
+	<li>To edit, type and distribute all written communications between board members.</li>
+	<li>To develop, evaluate, and update the constitution, policies, and operating procedures on a yearly basis.</li>
+	<li>To serve as the official representative of the organization in the absence of the President and Vice President.</li>
+	<li>To take attendance at all meetings, events etc.</li>
+	<li>To plan, coordinate and implement all aspects of all programs during the year.</li>
+	<li>To attend all Executive Board meetings.</li>
+	<li>To perform other duties as delegated by the President.</li>
+	<li>To maintain an up to date mailing list of all correspondences.</li>
+	<li>Serve as Chair of the Publicity Committee.</li>
+</ol>
+<h3>Treasurer</h3>
+<ol class="bl">
+	<li>To work closely with the Advisor(s) in all fiscal matters.</li>
+	<li>To assist in the preparation of the annual budget request.</li>
+	<li>To keep an up-to-date file on the organization concerning all budgetary matters.</li>
+	<li>To report all budgetary activities on the organization as they occur.</li>
+	<li>To discuss any problems with the President and Advisor(s).</li>
+	<li>Collect dues and maintain financial and membership records.</li>
+	<li>To initiate payment within five days or the receipt of invoice.</li>
+	<li>To plan, coordinate and implement all aspects of all programs during the year.</li>
+	<li>To attend all Executive Board meetings.</li>
+	<li>To perform duties as delegated by the President.</li>
+</ol>
+<h3>Removal of an officer</h3>
+<ol class="bl">
+	<li>If an officer is not performing the duties aforementioned or they fall below the criterion set forth by the Hunter USG bylaws, then the procedures set forth in Article IV, Sections 4 through 6 shall take place.</li>
+</ol>
+<p class="nb">NB. An unofficial "webmaster" position is often available on a per-semester basis depending on need and experience. If a webmaster cannot be found from the membership at any given time, the club must budget for website management.</p>
+
+<h2>VI. Chapter Sponsor</h2>
+<ol class="bl">
+	<li>The Sponsor shall be a faculty member or full-time staff member of your school. Each Student Chapter has one Sponsor.</li>
+	<li>The Student Chapter Sponsor shall be a voting member of ACM and either a member of the faculty or full-time staff of Hunter College.</li>
+	<li>The Student Chapter Sponsor shall be selected by the Executive Council immediately following the Election Meeting. The selection shall be confirmed by the school.</li>
+	<li>The Student Chapter Sponsor shall be generally responsible for the activities of the Chapter. Specifically, the Sponsor:
+		<ol>
+			<li>helps provide continuity from year to year as student leadership and personnel change;</li>
+			<li>promotes good student-faculty relationships;</li>
+			<li>helps maintain university standards in all activities of the Chapter;</li>
+			<li>exercises financial supervision, if necessary, by:
+				<ol>
+					<li>promoting prompt payment of bills and collection of dues, and</li>
+					<li>overseeing the settlement of all accounts in the event of dissolution of the Chapter; and</li>
+				</ol>
+			</li>
+			<li>represents the Chapter interests to the faculty and administration.</li>
+		</ol>
+</ol>
+
+<h2>VII. Committees</h2>
+<ol class="bl">
+	<li>The Standing Committees shall be:
+		<ol>
+			<li><strong>The Program Committee</strong>, which shall plan and arrange meetings of the Chapter in accordance with membership interests and the aims of this Chapter as set forth in Article II; and shall prepare a calendar of regularly scheduled meetings each year.</li>
+			<li><strong>The Publicity Committee</strong>, which shall maintain a mailing list, and publish and distribute all notices to the members of this Chapter; and shall provide additional publicity of Chapter activities as appropriate.</li>
+			<li><strong>The Election Committee</strong>, which shall screen candidates for Executive Board positions and serve as advisors in the case of an office being vacated. It is chaired by members of the Executive Board.</li>
+		</ol>
+	</li>
+	<li>Each Standing Committee shall consist of at least two voting members of this Chapter.</li>
+	<li>In addition to Standing Committees, Temporary Committees may be established by the Chapter President.</li>
+	<li>The President and members of both Standing and Temporary Committees are appointed by the Chapter President, except as otherwise provided in the bylaws. All committee appointments shall terminate at the end of the annual election semester.</li>
+	<li>Other temporary committees may be established by the executive Board.</li>
+</ol>
+<p class="nb">NB. In the absence of committee membership, either due to low overall member attendance and/or due to lack of general interest, the Executive Board functionally serves in lieu of a given committee.</p>
+
+<h2>VIII. Meetings</h2>
+<ol class="bl">
+	<li>Meetings shall be held as planned by the Program Committee.</li>
+	<li>The annual election meeting shall be held every May.
+		<ol>
+			<li>The Executive Board shall by consulting The Election Committee appoint candidates for office, nominate members for positions in the Executive Board.</li>
+			<li>After the nominations are closed, the voting members present shall vote.</li>
+			<li>In order for the nomination to stand, that member must satisfy requirements set forth in Article IV, Sections 2-3.</li>
+			<li>A majority of the votes cast for an office is necessary for election to that office.</li>
+			<li>The Secretary shall present the annual Chapter report.</li>
+		</ol>
+	</li>
+	<li>Notices shall be distributed to all members at least one week prior to any meeting.</li>
+	<li>There shall be at least five meetings each year.</li>
+</ol>
+
+<h2>IX. Disbursements and Dues</h2>
+<ol class="bl">
+	<li>Disbursements from the Treasury for Chapter expenditures shall be made by the Treasurer with authorization of both the President and at least one Faculty Advisor and shall be included in the minutes of its meetings.</li>
+	<li>Dues shall be fixed annually by the Executive Board.</li>
+</ol>
+
+<h2>X. Amendments and Procedures</h2>
+<ol class="bl">
+	<li>All proposed changes to these Chapter Bylaws shall be approved by the President of the Membership Activities Board and the ACM Constitution and Bylaws Committee before being presented to the Chapter membership for a vote.</li>
+	<li>Two-thirds of the voting members present and voting shall be required to adopt the amendment(s).</li>
+	<li>Rulings on any point of procedure not included in these bylaws shall be made by the President with approval of the faculty Advisor(s).</li>
+</ol>
+<p class="nb">NB. Unless otherwise noted, status as an ACM student chapter is subject to ACM policies, status as a Hunter College undergraduate student organization is subject to Hunter USG policies, and Hunter USG policies usually overrule ACM policies.</p>
+
+<h2>XI. Dissolution</h2>
+<ol class="bl">
+	<li>Dissolution of this Chapter by consent of the members shall consist of unanimous agreement of all its officers together with a majority vote at a meeting which has been publicized in advance to all members of the Chapter for the purpose of taking this vote.</li>
+	<li>Should this Chapter be dissolved, its assets and liabilities shall be transferred to ACM and shall be supervised by the Membership Activities Board.</li>
+</ol>
+<p class="nb">NB. If the club fails to meet either ACM student chapter minimum requirements or Hunter USG minimum requirements, its status as an active ACM student chapter and/or as an active club at Hunter College is subject to those deactivation policies.</p>
+
+<?php require_once(dirname(__FILE__).'/scripts/bottom.php') ?>
