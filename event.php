237d597fc7ef2949c8e45f2cab30cea34f330f80
Alex Rosario <neoexmachina@gmail.com>
2014-09-25 18:33:16 -0400

    Event single cleanup, speaker/location

--- event.php
+++ event.php
@@ -5,8 +5,7 @@ require_once(dirname(__FILE__).'/scripts/top.php');
 
 $event = $events[$_GET['event']];
 if($event) {
-	$evt_minutes = "";
-	$evt_msg = "";	
+	$evt_msg = "";
 	$evt_li = "\t\t".'<li><a href="%s">%s</a></li>'."\n";
 
 	if(!empty($event['url'])) $evt_msg .= sprintf($evt_li, $event['url'], 'Link');
@@ -15,27 +14,26 @@ if($event) {
 	if(!empty($event['slides'])) $evt_msg .= sprintf($evt_li, $event['slides'], 'Slides');
 	if(!empty($event['nda'])) $evt_msg .= sprintf($evt_li, $event['nda'], 'NDA');
 	if(!empty($event['ip'])) $evt_msg .= sprintf($evt_li, $event['ip'], 'IP');
-	if(!empty($event['survey'])) $evt_msg .= sprintf($evt_li, $event['survey'], 'Survey');	
-	if(!empty($evt_msg)) $evt_msg = "\t<ul>\n{$evt_msg}\t</ul>\n";
+	if(!empty($event['survey'])) $evt_msg .= sprintf($evt_li, $event['survey'], 'Survey');
+	if (!empty($evt_msg)) $evt_msg = "\t<ul>\n{$evt_msg}\t</ul>\n";
 ?>
 <div class='event'>
 	<h1 class='event-name'><?=$event['title']?></h1>
-	<h2 class='event-info'>Location: <strong><?=$event['location']?></h1></strong>
+	<h2 class='event-info'>Location: <em><?=$event['location']?></em></h2>
 	<h2 class='event-info'>
-		<em class='event-date'><?=$event['date']->format('Y/m/d')?></em> -
-		<strong class='event-guest'><?=stripslashes($event['speaker'])?></strong>
+		<em class='event-date'><?=$event['date']->format('Y/m/d')?></em>
+		<?php if (!empty($event['speaker'])) {?>- <strong class='event-guest'><?=stripslashes($event['speaker'])?></strong><?php } ?>
 	</h2>
-	
 <?=$evt_msg?>
-	<?=stripslashes(\Michelf\Markdown::defaultTransform($event['description']))?>	
+	<?=stripslashes(\Michelf\Markdown::defaultTransform($event['description']))?>
+</div>
 <?php
 	if (!empty($event['minutes'])) {?>
-		<div class="minutes" id="minutes">
-			<h3>Minutes</h3>
-			<?=stripslashes(\Michelf\Markdown::defaultTransform($event['minutes']))?>
-		</div>
-	<?php } ?>
+<div class="minutes" id="minutes">
+	<h3>Minutes</h3>
+	<?=stripslashes(\Michelf\Markdown::defaultTransform($event['minutes']))?>
 </div>
+<?php }
 }
 else if (!empty($_GET['event'])) {
 ?>
