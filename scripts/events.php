c21168ffa3c7cd11c76072df705d8ab00cfc5008
Robert O'Connor <rob@oconnor.ninja>
2014-09-25 19:57:17 -0400

    Refactor biographical details about speakers into its own attribute in the events yaml file

--- scripts/events.php
+++ scripts/events.php
@@ -42,7 +42,8 @@ function render_event_list_item($event, $root){
 		$event['date']->format('Y/m/d'),
 		$event['time']->format('g:ia'),
 		!empty($event['location'])?' in <strong class="event-guest">'.$event['location'].'</strong>':'',
-		!empty($event['speaker'])?' feat. <strong class="event-guest">'.stripslashes($event['speaker']).'</strong>':''
+		!empty($event['speaker'])?' feat. <strong class="event-guest">'.stripslashes($event['speaker']).'</strong>':'',
+        !empty($event['bio']) ? stripcslashes($event['bio']) : ''
 	);
 }
 
