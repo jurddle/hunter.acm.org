7d79df2f76a2f52d8d89926915542704badeecea
Alex Rosario <neoexmachina@gmail.com>
2014-09-25 18:32:09 -0400

    Contact cleanup

--- contact.php
+++ contact.php
@@ -6,29 +6,20 @@ require_once(dirname(__FILE__).'/scripts/top.php');
 
 <h1>Contact Us</h1>
 
-<p>
-  Questions, comments, or concerns? Feel free to contact us if there's anything ACM related you want to talk about.
-</p>
+<p>Questions, comments, or concerns? Feel free to contact us if there's anything ACM related you want to talk about.</p>
 
 <div class="row">
-  <div class="col-md-4">
-    <h2>IRC</h2>
-      Join us at <a href="irc://freenode/#hunter-acm">#hunter-acm on freenode</a>
-    <br /><a href="http://www.irchelp.org/irchelp/irctutorial.html#intro" class="irc_link">What is IRC?</a>
-      <br /><a class="btn btn-success" href="http://webchat.freenode.net/?channels=%23hunter-acm">Chat Now</a>
-  </div>
-
-  <div class="col-md-6">
-    <h2>Email</h2>
-    <p>
-    <a href="mailto:huntercollege.acm.chapter@gmail.com">huntercollege.acm.chapter AT gmail DOT com</a>
-    </p>
-    <p>
-      All chapter officers have access to this account.
-    </p>
-  </div>
+	<div class="col-md-6">
+		<h2>IRC</h2>
+		<p>Join us at <a href="irc://freenode/#hunter-acm">#hunter-acm on freenode</a></p>
+		<p><small><a href="http://www.irchelp.org/irchelp/irctutorial.html#intro" class="irc_link">What is IRC?</a></small></p>
+		<p><a class="btn btn-success" href="http://webchat.freenode.net/?channels=%23hunter-acm">Chat Now</a></p>
+	</div>
+	<div class="col-md-6">
+		<h2>Email</h2>
+		<p><a href="mailto:huntercollege.acm.chapter@gmail.com">huntercollege.acm.chapter AT gmail DOT com</a></p>
+		<p>All chapter officers have access to this account.</p>
+	</div>
 </div>
 
 <?php require_once(dirname(__FILE__).'/scripts/bottom.php') ?>
-
-</section>
