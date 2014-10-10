b6cdf24606f6045c1d689511772ab3f3af2e772f
Robert O'Connor <rob@oconnor.ninja>
2014-10-10 19:51:45 -0400

    tweak

--- scripts/bottom.php
+++ scripts/bottom.php
@@ -2,6 +2,6 @@
 <footer id='ftr'><div class='container'>
 <?php if ($is_cuny) { ?>	<p id='cuny'><a href='http://www.cuny.edu/' title='Hunter is CUNY'>Hunter is CUNY</a></p><?php } ?>
 	<p>&copy; 2014 ACM @ Hunter</p>
-	<p>Special thanks to ACM @ PSU for inspiration ;) Checkout our <a href=''http://github.com/HunterACM/hunter.acm.org' target='_blank'>source code</a></p>
+	<p>Special thanks to ACM @ PSU for inspiration ;) Checkout our <a href='http://github.com/HunterACM/hunter.acm.org' target='_blank'>source code</a></p>
 </div></footer>
 </body></html>
