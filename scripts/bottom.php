85aa0e1c612f77d6e7ebac8da55513f729d1c752
Alex Rosario <neoexmachina@gmail.com>
2014-10-11 18:59:20 -0400

    Reposition link

--- scripts/bottom.php
+++ scripts/bottom.php
@@ -1,7 +1,7 @@
 </div></article>
 <footer id='ftr'><div class='container'>
 <?php if ($is_cuny) { ?>	<p id='cuny'><a href='http://www.cuny.edu/' title='Hunter is CUNY'>Hunter is CUNY</a></p><?php } ?>
-	<p>&copy; 2014 ACM @ Hunter</p>
-	<p>Special thanks to ACM @ PSU for inspiration ;) Checkout our <a href='http://github.com/HunterACM/hunter.acm.org' target='_blank'>source code</a></p>
+	<p>&copy; 2014 ACM @ Hunter :: <a href='http://github.com/HunterACM/hunter.acm.org' target='_blank'>Checkout our source code</a></p>
+	<p>Special thanks to ACM @ PSU for inspiration ;)</p>
 </div></footer>
 </body></html>
